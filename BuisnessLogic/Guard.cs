ff476117389e9243ea9fddddaba45076b497f723
neurxdreamz <savva.kononov.2018@mail.ru>
2026-09-24 19:56:35 +0300

    Добавил класс Guard для обработки предусловий

---
+++ BuisnessLogic/Guard.cs
@@ -0,0 +1,23 @@
+﻿namespace BuisnessLogic
+{
+    public static class Guard
+    {
+        /// <summary>
+        /// метод проверяет условие и выбрасывает исключение с указанным сообщенимем еслии он не выполняется
+        /// </summary>
+        public static void Requires(bool condition, string message)
+        {
+            if (!condition)
+            {
+                throw new PreViolationException(message);
+            }
+        }
+        /// <summary>
+        /// пользовательское исключение для отделения контракта от других системных ошибок
+        /// </summary>
+        public class PreViolationException : Exception
+        {
+            public PreViolationException(string message) : base(message) { }
+        }
+    }
+}
