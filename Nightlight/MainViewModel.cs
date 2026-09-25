7321121909e80fd77744d972e4e041b082deaad6
neurxdreamz <savva.kononov.2018@mail.ru>
2026-09-25 13:37:27 +0300

    Добавил MainViewModel

---
+++ Nightlight/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using BuisnessLogic;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+public partial class MainViewModel : ObservableObject
+{
+  
+    public ObservableCollection<object> Operations { get; }
+
+   
+    [ObservableProperty]
+    private object _selectedOperation;
+
+    public MainViewModel(NightlightModel model)
+    {
+        Operations = new ObservableCollection<object>
+        {
+            new AutoToggleViewModel(model),
+            new SetBrightnessViewModel(model),
+            new SleepTimerViewModel(model)
+        };
+
+       
+        SelectedOperation = Operations[0];
+    }
+}
